4506580d96e3e88d501cb489827118653cd404e3
FelixM <felix.museng@binus.ac.id>
2023-03-16 23:34:42 +0800

    implemented router, navbar, styled components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter , Routes } from 'react-router-dom'
+
+import LoginPage from "./components/auth/LoginPage";
+import RegisterPage from "./components/auth/RegisterPage";
+import UserDetail from "./components/auth/UserDetail";
+import Home from './components/pages/Home';
+import Layout from './components/pages/Layout';
 
 function App() {
+  // const [] = useState([])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/regis" element={<RegisterPage/>}/>
+        </Route>
+      </Routes>
+      <UserDetail />
+    </BrowserRouter>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-QHcKHrKSEXRz4lvI1adTdYj5-op1jX4",
+  authDomain: "garuda-redesign.firebaseapp.com",
+  projectId: "garuda-redesign",
+  storageBucket: "garuda-redesign.appspot.com",
+  messagingSenderId: "21476304562",
+  appId: "1:21476304562:web:9a6206443e981d8af64bab"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app)
+export default app;
